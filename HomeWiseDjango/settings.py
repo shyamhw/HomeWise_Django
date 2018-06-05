4dc4c12e8f215a50ff4de4a77ff10a572ccfb08e
Visrut Sudhakar <visrut@cs.unc.edu>
2018-06-05 16:13:01 +0000

    Default config for EB

--- HomeWiseDjango/settings.py
+++ HomeWiseDjango/settings.py
@@ -87,15 +87,15 @@ WSGI_APPLICATION = 'HomeWiseDjango.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'remote': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': os.environ['RDS_DB_NAME'],
+    #     'USER': os.environ['RDS_USERNAME'],
+    #     'PASSWORD': os.environ['RDS_PASSWORD'],
+    #     'HOST': os.environ['RDS_HOSTNAME'],
+    #     'PORT': os.environ['RDS_PORT'],
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
-    },
-    'local': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
