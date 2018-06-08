1e1ca9308acc31fb679acee3729fe9d43ec80d68
Visrut Sudhakar <visrut@cs.unc.edu>
2018-06-08 14:25:29 +0000

    Corrected env variables

--- HomeWiseDjango/settings.py
+++ HomeWiseDjango/settings.py
@@ -101,8 +101,8 @@ DATABASES = {
 
 # Email Settings
 EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
-EMAIL_HOST_USER = os.environ[SES_USER]
-EMAIL_HOST_PASSWORD = os.environ[SES_PASSWORD]
+EMAIL_HOST_USER = os.environ['SES_USER']
+EMAIL_HOST_PASSWORD = os.environ['SES_PASSWORD']
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
