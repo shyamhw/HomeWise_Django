97199e82246519c244da51213e95bb01616953a0
Visrut Sudhakar <secure@visrut.com>
2018-06-29 15:30:29 -0400

    Models reflect new Auth strategy. User model and custom manager class. Fields updated to reflect encapsulation.

--- Agent/admin.py
+++ Agent/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from . import models
-# Register your models here.
-admin.site.register(models.Agent)
-admin.site.register(models.Client)
-admin.site.register(models.Step)
+
 admin.site.register(models.VendorRegion)
 admin.site.register(models.Tag)
 admin.site.register(models.MLSRegion)
+admin.site.register(models.Role)
+admin.site.register(models.User)
+admin.site.register(models.Agent)
+admin.site.register(models.Client)
+admin.site.register(models.Step)
 admin.site.register(models.Vendor)
-admin.site.register(models.User, UserAdmin)
