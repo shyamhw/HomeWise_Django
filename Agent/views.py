c1ba2248a5975f8271645d6abdbc5b48c0d9220d
shyam <shyam@joinhomewise.com>
2018-06-28 22:30:19 -0400

    fixed save bug

--- Agent/views.py
+++ Agent/views.py
@@ -504,12 +504,14 @@ class AddStep(APIView):
         new_total_steps = client.total_steps + 1
         new_steps_complete = client.steps_complete
         new_steps_percentage = new_total_steps / new_steps_complete
+        client.total_steps = new_total_steps
+        client.steps_percentage = new_steps_percentage
         client.save()
 
         try:
             c = Client.objects.get(id=id)
         except:
-            return Response("Not a Client", status=status.HTTP_400_BAD_REQUEST)
+            return Response("Not a Client 2", status=status.HTTP_400_BAD_REQUEST)
 
         step = Step(client=c, ordering=1, name=newStepName, complete=False, agent_email=agent_email, date=date)
         step.save()
